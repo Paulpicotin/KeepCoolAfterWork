6e01c139aaa2cb94b2c4d23d1489975139b796d8
Paulpicotin <44963927+Paulpicotin@users.noreply.github.com>
2020-09-28 10:21:38 +0200

    first commit

---
+++ app/src/main/java/com/example/keepcoolafterwork/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.keepcoolafterwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
