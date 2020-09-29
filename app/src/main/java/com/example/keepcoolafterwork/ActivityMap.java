88dced57151d9e813af3f6433450dd7a3bd7677e
aymeric <vigoureux@intechinfo.fr>
2020-09-29 11:19:51 +0200

    build gradle

--- app/src/main/java/com/example/keepcoolafterwork/ActivityMap.java
+++ app/src/main/java/com/example/keepcoolafterwork/ActivityMap.java
@@ -1,6 +1,14 @@
 package com.example.keepcoolafterwork;
 
 import android.os.Bundle;
+import android.location.Location;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
