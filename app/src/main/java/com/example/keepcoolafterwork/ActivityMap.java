6534b3396d52a4d4c987091f4fb47010f3c50f01
aymeric <vigoureux@intechinfo.fr>
2020-10-05 08:41:45 +0200

    map ready

--- app/src/main/java/com/example/keepcoolafterwork/ActivityMap.java
+++ app/src/main/java/com/example/keepcoolafterwork/ActivityMap.java
@@ -1,7 +1,8 @@
 package com.example.keepcoolafterwork;
 
 import android.os.Bundle;
-import android.location.Location;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,13 +11,24 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class ActivityMap extends AppCompatActivity {
+public class ActivityMap extends AppCompatActivity
+        implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
