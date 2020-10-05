57814667d1abec474479c94f6188914ad51bd064
Paulpicotin <44963927+Paulpicotin@users.noreply.github.com>
2020-10-05 21:10:31 +0200

    CONTACTS_LOADER_ID

--- app/src/main/java/com/example/keepcoolafterwork/MainActivity.java
+++ app/src/main/java/com/example/keepcoolafterwork/MainActivity.java
@@ -1,40 +1,41 @@
 package com.example.keepcoolafterwork;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.CursorLoader;
-import androidx.loader.content.Loader;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.ContentResolver;
-import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
-/*public class MainActivity extends AppCompatActivity {
 
-    private static  int CONTACTS_LOADER_ID  ;
-    private static  String TAG  ;
+public class MainActivity extends AppCompatActivity {
+
+
+    private  String TAG  ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Prepare the loader.  Either re-connect with an existing one,
-        // or start a new one.
-        getLoaderManager().initLoader(CONTACTS_LOADER_ID,
-                null,
-                this);
+       RequestPermission();
     }
+    private boolean RequestPermission() {
+        if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            getContactList();
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+
     private void getContactList() {
         ContentResolver cr = getContentResolver();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
@@ -68,84 +69,6 @@ import java.util.List;
             cur.close();
         }
     }
-}*/
-public class MainActivity extends Activity
-        implements LoaderManager.LoaderCallbacks<Cursor> {
-
-    private static final int CONTACTS_LOADER_ID = 1;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Prepare the loader.  Either re-connect with an existing one,
-        // or start a new one.
-        getLoaderManager().initLoader(CONTACTS_LOADER_ID,
-                null,
-                (android.app.LoaderManager.LoaderCallbacks<Object>) this)
-                ;
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        // This is called when a new Loader needs to be created.
-
-        if (id == CONTACTS_LOADER_ID) {
-            return contactsLoader();
-        }
-        return null;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        //The framework will take care of closing the
-        // old cursor once we return.
-        List<String> contacts = contactsFromCursor(cursor);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        // This is called when the last Cursor provided to onLoadFinished()
-        // above is about to be closed.  We need to make sure we are no
-        // longer using it.
-    }
-
-    private Loader<Cursor> contactsLoader() {
-        Uri contactsUri = ContactsContract.Contacts.CONTENT_URI; // The content URI of the phone contacts
-
-        String[] projection = {                                  // The columns to return for each row
-                ContactsContract.Contacts.DISPLAY_NAME
-        } ;
-
-        String selection = null;                                 //Selection criteria
-        String[] selectionArgs = {};                             //Selection criteria
-        String sortOrder = null;                                 //The sort order for the returned rows
-
-        return new CursorLoader(
-                getApplicationContext(),
-                contactsUri,
-                projection,
-                selection,
-                selectionArgs,
-                sortOrder);
-    }
-
-    private List<String> contactsFromCursor(Cursor cursor) {
-        List<String> contacts = new ArrayList<String>();
-
-        if (cursor.getCount() > 0) {
-            cursor.moveToFirst();
-
-            do {
-                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                contacts.add(name);
-            } while (cursor.moveToNext());
-        }
-
-        return contacts;
-    }
-
 }
 
 
